4dd482f7dc05fc875f035211e62655d499463cd6
Hao Li <lhrcplanes@qq.com>
2020-06-22 22:20:27 -0700

    include new files

--- src/VisDyn.jl
+++ src/VisDyn.jl
@@ -1,5 +1,11 @@
 module VisDyn
 
+include("colorMap.jl")
+export cMap, cMap_def, cNormalize, qcMap_def
+export plotTrace!, plotScatter!, plotTrajectory!
+include("plotGradient.jl")
+export plotVF!, plotNullCline!, plotGradient!, plotValongCycle!, circularCycle
+
 # Write your package code here.
 
 end
